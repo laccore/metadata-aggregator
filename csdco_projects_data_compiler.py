3b2f5af75a23679bacc76f9996098e3a67c426ef
Alexander Stone <enderu@gmail.com>
2018-01-16 15:03:22 -0600

    Move to function, add __main__ check.

--- csdco_projects_data_compiler.py
+++ csdco_projects_data_compiler.py
@@ -24,84 +24,97 @@ import datetime
 
 start_time = timeit.default_timer()
 
-debug_projects = []
 
-infile = sys.argv[1]
-if len(sys.argv) > 2:
-    outfile = sys.argv[2]
-else:
-    outfile = 'project_data_' + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.csv'
+def aggregate_metadata(infile, outfile, exclude_projects, debug_projects):
+    with open(infile, 'r', encoding='utf-8-sig') as f:
+        rawdata = f.read().splitlines()
 
-with open(infile, 'r', encoding='utf-8-sig') as f:
-	rawdata = f.read().splitlines()
+    data = []
 
-data = []
+    # The Excel file often exports a huge amount of empty rows of varying number of columns. This ignores those.
+    for r in rawdata:
+        if r.replace(',','') != '':
+            data += [r]
 
-# The Excel file often exports a huge amount of empty rows of varying number of columns. This ignores those.
-for r in rawdata:
-    if r.replace(',','') != '':
-        data += [r]
+    data = data[1:]     # ignore hearder
+    rdata = []
 
-data = data[1:]     # ignore hearder
-rdata = []
+    for r in csv.reader(data, quotechar='"', delimiter=','):
+        rdata += [r[:32]]
 
-for r in csv.reader(data, quotechar='"', delimiter=','):
-    rdata += [r[:32]]
-
-expeditions = set()
-countries = {}
-states = {}
-featureNames = {}
-pis = {}
+    expeditions = set()
+    countries = {}
+    states = {}
+    featureNames = {}
+    pis = {}
 
 
-for r in rdata:
-    e = r[7]
-    expeditions.add(e)
+    for r in rdata:
+        e = r[7]
+        expeditions.add(e)
 
-    c = r[17]
-    s = r[18]
-    f = r[5].replace(',','')
-    p = r[20].split(', ')
+        c = r[17]
+        s = r[18]
+        f = r[5].replace(',','')
+        p = r[20].split(', ')
 
-    if e in debug_projects:
-        print('expedition: {}\ncountry: {}\nstate: {}\nfeatures: {}\npis: {}\n'.format(e,c,s,f,p))
+        if e in debug_projects:
+            print('expedition: {}\ncountry: {}\nstate: {}\nfeatures: {}\npis: {}\n'.format(e,c,s,f,p))
+        
+        if e not in countries:
+            countries[e] = [c]
+            states[e] = [s]
+            featureNames[e] = [f]
+            pis[e] = p
+        else:
+            countries[e] += [c]
+            states[e] += [s]
+            featureNames[e] += [f]
+            pis[e] += p
+
+
+    # Cleanup and deduplicate
+    empty = {'', 'n/a', 'N/A'}
+    for e in expeditions:
+        countries[e] = list(set(countries[e])-empty)
+        states[e] = list(set(states[e])-empty)
+        featureNames[e] = list(set(featureNames[e])-empty)
+        pis[e] = list(set(pis[e])-empty)
+
+
+    with open(outfile, 'w', encoding='utf-8-sig') as f:
+        f.write('\"PROJECT ID\",\"LOCATION\",\"NAMED FEATURE\",\"INVESTIGATOR\"\n')
+
+        for e in sorted(expeditions-set(exlcude_projects)):
+            l = ','.join(sorted(countries[e])+sorted(states[e]))
+            nf = ','.join(sorted(featureNames[e]))
+            i = ','.join(pis[e])
+
+            f.write('\"' + e + '\",\"' + l + '\",\"' + nf + '\",\"' + i + '\"\n')
+
+
+    print('{0} projects found.'.format(len(expeditions-set(exlcude_projects))))
+    print('Aggregated data written to {0}.'.format(outfile))
+
+    end_time = timeit.default_timer()
+    print('Completed in {0} seconds.'.format(round(end_time-start_time,2)))
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print('ERROR: invalid parameters')
+        exit(1)
     
-    if e not in countries:
-        countries[e] = [c]
-        states[e] = [s]
-        featureNames[e] = [f]
-        pis[e] = p
+    infile = sys.argv[1]
+
+    if len(sys.argv) > 2:
+        outfile = sys.argv[2]
     else:
-        countries[e] += [c]
-        states[e] += [s]
-        featureNames[e] += [f]
-        pis[e] += p
+        outfile = 'project_data_' + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.csv'
 
+    # projects to exclude from export, e.g., ocean drilling projects
+    exlcude_projects = ['AT15','ORCA','B0405','B0506']
+        
+    # print troubleshooting info
+    debug_projects = []
 
-# Cleanup and deduplicate
-empty = {'', 'n/a', 'N/A'}
-for e in expeditions:
-    countries[e] = list(set(countries[e])-empty)
-    states[e] = list(set(states[e])-empty)
-    featureNames[e] = list(set(featureNames[e])-empty)
-    pis[e] = list(set(pis[e])-empty)
-
-
-
-with open(outfile, 'w', encoding='utf-8-sig') as f:
-    f.write('\"PROJECT ID\",\"LOCATION\",\"NAMED FEATURE\",\"INVESTIGATOR\"\n')
-
-    for e in sorted(expeditions):
-        l = ','.join(sorted(countries[e])+sorted(states[e]))
-        nf = ','.join(sorted(featureNames[e]))
-        i = ','.join(pis[e])
-
-        f.write('\"' + e + '\",\"' + l + '\",\"' + nf + '\",\"' + i + '\"\n')
-
-
-print('{0} projects found.'.format(len(expeditions)))
-print('Aggregated data written to {0}.'.format(outfile))
-
-end_time = timeit.default_timer()
-print('Completed in {0} seconds.'.format(round(end_time-start_time,2)))
+    aggregate_metadata(infile, outfile, exlcude_projects, debug_projects)
