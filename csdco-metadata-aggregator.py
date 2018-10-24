83a13c6cf9c20ef1ed53e5b68bd0afc959f131c2
Alexander Stone <enderu@gmail.com>
2018-10-24 14:28:05 -0500

    Lots of updates to make it work with our new sqlite db. It turns out the db returning None for empty cells (instead of the empty string from a csv) had a lot of repercussions. For what we're doing, it turned out to be easier to just convert them to empty strings anyway.

--- csdco-metadata-aggregator.py
+++ csdco-metadata-aggregator.py
@@ -32,8 +32,9 @@ TODO:
 
 import sys
 import timeit
-import csv
 import datetime
+import sqlite3
+import collections
 
 start_time = timeit.default_timer()
 
@@ -43,65 +44,53 @@ def aggregate_metadata(infile, outfile, **kwargs):
         exclude_projects = kwargs['exclude_projects']
     else:
         exclude_projects = []
-
+    
     if 'debug_projects' in kwargs:
         debug_projects = kwargs['debug_projects']
     else:
         debug_projects = []
     
-    with open(infile, 'r', encoding='utf-8-sig') as f:
-        rawdata = f.read().splitlines()
+    # with open(infile, 'r', encoding='utf-8-sig') as f:
+    #     rawdata = f.read().splitlines()
 
-    # The Excel file often exports a huge amount of empty rows of varying number of columns. This ignores those.
-    data = [r for r in rawdata[1:] if r.replace(',','') != '']
+    # # The Excel file often exports a huge amount of empty rows of varying number of columns. This ignores those.
+    # data = [r for r in rawdata[1:] if r.replace(',','') != '']
 
-    rdata = [r[:32] for r in csv.reader(data, quotechar='"', delimiter=',')]
+    # rdata = [r[:32] for r in csv.reader(data, quotechar='"', delimiter=',')]
+
+    conn = sqlite3.connect(infile)
+    c = conn.cursor()
 
     expeditions = set()
-    countries = {}
-    states = {}
-    feature_names = {}
-    pis = {}
+    countries = collections.defaultdict(set)
+    states = collections.defaultdict(set)
+    feature_names = collections.defaultdict(set)
+    pis = collections.defaultdict(list)
 
-
-    for r in rdata:
-        e = r[7]
+    for r in c.execute("SELECT Expedition, Country, State_Province, Location, PI FROM boreholes"):
+        [e, c, s, f, p] = r
         expeditions.add(e)
 
-        c = r[17].replace(',','')
-        s = r[18].replace(',','')
-        f = r[5].replace(',','')
-        p = r[20].split(', ')
+        c = c.replace(',','') if c else ''
+        s = s.replace(',','') if s else ''
+        f = f.replace(',','') if f else ''
+        p = p.split(', ') if p else ''
 
         if e in debug_projects:
             print('expedition: {}\ncountry: {}\nstate: {}\nfeature name: {}\npis: {}\n'.format(e,c,s,f,p))
         
-        if e not in countries:
-            countries[e] = [c]
-            states[e] = [s]
-            feature_names[e] = [f]
-            pis[e] = p
-        else:
-            countries[e] += [c]
-            states[e] += [s]
-            feature_names[e] += [f]
-            pis[e] += p
-
-
-    # Cleanup and deduplicate
-    empty = {'', 'n/a', 'N/A', ' '}
-    for e in expeditions:
-        countries[e] = list(set(countries[e])-empty)
-        states[e] = list(set(states[e])-empty)
-        feature_names[e] = list(set(feature_names[e])-empty)
-        seen_pis = set()
-        pis[e] = [pi for pi in pis[e] if not (pi in seen_pis or seen_pis.add(pi) or pi in empty)]
+        countries[e].add(c)
+        states[e].add(s)
+        feature_names[e].add(f)
+        for pi in p:
+            if pi not in pis[e]:
+                pis[e].append(pi)
 
 
     with open(outfile, 'w', encoding='utf-8-sig') as f:
         f.write('\"PROJECT ID\",\"LOCATION\",\"NAMED FEATURE\",\"INVESTIGATOR\"\n')
 
-        for e in sorted(expeditions-set(exlcude_projects)):
+        for e in sorted(expeditions-set(exclude_projects)):
             l = ','.join(sorted(countries[e])+sorted(states[e]))
             nf = ','.join(sorted(feature_names[e]))
             i = ','.join(pis[e])
@@ -109,7 +98,7 @@ def aggregate_metadata(infile, outfile, **kwargs):
             f.write('\"' + e + '\",\"' + l + '\",\"' + nf + '\",\"' + i + '\"\n')
 
 
-    print('{0} projects found.'.format(len(expeditions-set(exlcude_projects))))
+    print('{0} projects found.'.format(len(expeditions-set(exclude_projects))))
     print('Aggregated data written to {0}.'.format(outfile))
 
     end_time = timeit.default_timer()
@@ -118,8 +107,8 @@ def aggregate_metadata(infile, outfile, **kwargs):
 if __name__ == '__main__':
     if len(sys.argv) < 2 or len(sys.argv) > 3:
         print('ERROR: invalid parameters. Exiting.')
-        print('Usage (e.g.): python csdco-metadata-aggregator.py LacCore_Holes_20180116.csv')
-        print('              python csdco-metadata-aggregator.py LacCore_Holes_20180116.csv project_list_for_website.csv')
+        print('Usage (e.g.): python csdco-metadata-aggregator.py CSDCO.sqlite3')
+        print('              python csdco-metadata-aggregator.py CSDCO.sqlite3 project_list_for_website.csv')
         exit(1)
     
     infile = sys.argv[1]
