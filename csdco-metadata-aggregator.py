9624332e6b2d4c97cd206a849b6e6fef1f5b5083
Alexander Stone <enderu@gmail.com>
2018-10-24 15:26:24 -0500

    Fixed a few edge cases, switched to argparse, removed old code, removed and updated some comments.

--- csdco-metadata-aggregator.py
+++ csdco-metadata-aggregator.py
@@ -1,22 +1,12 @@
 
 '''
-This script takes as input a CSV export of our LacCore_Holes_YYYYMMDD.xlsx file and 
-combines locations (country and state), feature names, and PIs by project code, with
-some general data cleanup along the way. It then exports all that as a csv.
+This script takes as input the CSDCO sqlite database and combines locations (country and state),
+feature names, and PIs by project code, and then exports all that as a csv.
 
 The exported csv can then be imported into Drupal, which updates the project and map
 databases on our website.
 
-Field           Column Number
--------------   -------------
-Expedition      7
-Location        5
-Country         17
-State_Province  18
-PI              20
-
-
-Spreadsheet Name  Excel Name
+Spreadsheet Name  Database Columns
 ----------------  -----------------
 PROJECT ID        Expedition
 LOCATION          Country,State_Province
@@ -27,17 +17,14 @@ TODO:
 * Allow entry of a starting row, i.e., all data at and beyond which is new and only handle that data.
 * Build GUI
 * Use argparse
-* use numpy to handle Excel?
 '''
 
-import sys
 import timeit
 import datetime
 import sqlite3
 import collections
-
-start_time = timeit.default_timer()
-
+import argparse
+import os
 
 def aggregate_metadata(infile, outfile, **kwargs):
     if 'exclude_projects' in kwargs:
@@ -49,14 +36,8 @@ def aggregate_metadata(infile, outfile, **kwargs):
         debug_projects = kwargs['debug_projects']
     else:
         debug_projects = []
-    
-    # with open(infile, 'r', encoding='utf-8-sig') as f:
-    #     rawdata = f.read().splitlines()
 
-    # # The Excel file often exports a huge amount of empty rows of varying number of columns. This ignores those.
-    # data = [r for r in rawdata[1:] if r.replace(',','') != '']
 
-    # rdata = [r[:32] for r in csv.reader(data, quotechar='"', delimiter=',')]
 
     conn = sqlite3.connect(infile)
     c = conn.cursor()
@@ -71,21 +52,21 @@ def aggregate_metadata(infile, outfile, **kwargs):
         [e, c, s, f, p] = r
         expeditions.add(e)
 
-        c = c.replace(',','') if c else ''
-        s = s.replace(',','') if s else ''
-        f = f.replace(',','') if f else ''
-        p = p.split(', ') if p else ''
+        c = c.replace(',','') if c else None
+        s = s.replace(',','') if s else None
+        f = f.replace(',','') if f else None
+        p = p.split(', ') if p else None
 
         if e in debug_projects:
             print('expedition: {}\ncountry: {}\nstate: {}\nfeature name: {}\npis: {}\n'.format(e,c,s,f,p))
         
-        countries[e].add(c)
-        states[e].add(s)
-        feature_names[e].add(f)
-        for pi in p:
-            if pi not in pis[e]:
-                pis[e].append(pi)
-
+        c and countries[e].add(c)
+        s and states[e].add(s)
+        f and feature_names[e].add(f)
+        if p:
+            for pi in p:
+                if pi not in pis[e]:
+                    pis[e].append(pi)
 
     with open(outfile, 'w', encoding='utf-8-sig') as f:
         f.write('\"PROJECT ID\",\"LOCATION\",\"NAMED FEATURE\",\"INVESTIGATOR\"\n')
@@ -97,26 +78,30 @@ def aggregate_metadata(infile, outfile, **kwargs):
 
             f.write('\"' + e + '\",\"' + l + '\",\"' + nf + '\",\"' + i + '\"\n')
 
-
     print('{0} projects found.'.format(len(expeditions-set(exclude_projects))))
     print('Aggregated data written to {0}.'.format(outfile))
 
-    end_time = timeit.default_timer()
-    print('Completed in {0} seconds.'.format(round(end_time-start_time,2)))
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2 or len(sys.argv) > 3:
-        print('ERROR: invalid parameters. Exiting.')
-        print('Usage (e.g.): python csdco-metadata-aggregator.py CSDCO.sqlite3')
-        print('              python csdco-metadata-aggregator.py CSDCO.sqlite3 project_list_for_website.csv')
-        exit(1)
+    #     print('ERROR: invalid parameters. Exiting.')
+    #     print('Usage (e.g.): python csdco-metadata-aggregator.py CSDCO.sqlite3')
+    #     print('              python csdco-metadata-aggregator.py CSDCO.sqlite3 project_list_for_website.csv')
+    #     exit(1)
     
-    infile = sys.argv[1]
 
-    if len(sys.argv) > 2:
-        outfile = sys.argv[2]
-    else:
-        outfile = 'project_data_' + datetime.datetime.now().strftime('%Y%m%d_%H%M%S') + '.csv'
+    # else:
+    #     outfile = 'project_data_' + datetime.datetime.now().strftime('%Y%m%d_%H%M%S') + '.csv'
+    parser = argparse.ArgumentParser(description='Aggregate fields from the CSDCO database by Expedition Drupal for publishing.')
+    parser.add_argument('db_file', type=str, help='Name of CSDCO database file.')
+    parser.add_argument('-f', type=str, help='Filename for export.')
+    args = parser.parse_args()
+
+    if not os.path.isfile(args.db_file):
+      print('ERROR: database file \'{}\' does not exist.\n'.format(args.db_file))
+      exit(1)
+
+    # Use filename if provided, else create using datetimestamp
+    outfile = args.f if args.f else 'project_data_' + datetime.datetime.now().strftime('%Y%m%d_%H%M%S') + '.csv'
 
     # projects to exclude from export, e.g., ocean drilling projects
     # TODO: allow passing of list via command line
@@ -126,4 +111,7 @@ if __name__ == '__main__':
     # TODO: allow passing of list via command line    
     debug_projects = []
 
-    aggregate_metadata(infile, outfile, exclude_projects=exclude_projects, debug_projects=debug_projects)
+    start_time = timeit.default_timer()
+    aggregate_metadata(args.db_file, outfile, exclude_projects=exclude_projects, debug_projects=debug_projects)
+    end_time = timeit.default_timer()
+    print('Completed in {0} seconds.'.format(round(end_time-start_time,2)))
