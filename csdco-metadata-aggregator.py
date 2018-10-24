f6bec6d44bceb1aa178bfc69144dbd41616a6970
Alexander Stone <enderu@gmail.com>
2018-10-24 15:28:43 -0500

    Switch from 4 to 2 space tabs

--- csdco-metadata-aggregator.py
+++ csdco-metadata-aggregator.py
@@ -1,4 +1,3 @@
-
 '''
 This script takes as input the CSDCO sqlite database and combines locations (country and state),
 feature names, and PIs by project code, and then exports all that as a csv.
@@ -26,81 +25,81 @@ import argparse
 import os
 
 def aggregate_metadata(infile, outfile, **kwargs):
-    if 'exclude_projects' in kwargs:
-        exclude_projects = kwargs['exclude_projects']
-    else:
-        exclude_projects = []
+  if 'exclude_projects' in kwargs:
+    exclude_projects = kwargs['exclude_projects']
+  else:
+    exclude_projects = []
+  
+  if 'debug_projects' in kwargs:
+    debug_projects = kwargs['debug_projects']
+  else:
+    debug_projects = []
+
+
+
+  conn = sqlite3.connect(infile)
+  c = conn.cursor()
+
+  expeditions = set()
+  countries = collections.defaultdict(set)
+  states = collections.defaultdict(set)
+  feature_names = collections.defaultdict(set)
+  pis = collections.defaultdict(list)
+
+  for r in c.execute("SELECT Expedition, Country, State_Province, Location, PI FROM boreholes"):
+    [e, c, s, f, p] = r
+    expeditions.add(e)
+
+    c = c.replace(',','') if c else None
+    s = s.replace(',','') if s else None
+    f = f.replace(',','') if f else None
+    p = p.split(', ') if p else None
+
+    if e in debug_projects:
+      print('expedition: {}\ncountry: {}\nstate: {}\nfeature name: {}\npis: {}\n'.format(e,c,s,f,p))
     
-    if 'debug_projects' in kwargs:
-        debug_projects = kwargs['debug_projects']
-    else:
-        debug_projects = []
+    c and countries[e].add(c)
+    s and states[e].add(s)
+    f and feature_names[e].add(f)
+    if p:
+      for pi in p:
+        if pi not in pis[e]:
+          pis[e].append(pi)
 
+  with open(outfile, 'w', encoding='utf-8-sig') as f:
+    f.write('\"PROJECT ID\",\"LOCATION\",\"NAMED FEATURE\",\"INVESTIGATOR\"\n')
 
+    for e in sorted(expeditions-set(exclude_projects)):
+      l = ','.join(sorted(countries[e])+sorted(states[e]))
+      nf = ','.join(sorted(feature_names[e]))
+      i = ','.join(pis[e])
 
-    conn = sqlite3.connect(infile)
-    c = conn.cursor()
+      f.write('\"' + e + '\",\"' + l + '\",\"' + nf + '\",\"' + i + '\"\n')
 
-    expeditions = set()
-    countries = collections.defaultdict(set)
-    states = collections.defaultdict(set)
-    feature_names = collections.defaultdict(set)
-    pis = collections.defaultdict(list)
-
-    for r in c.execute("SELECT Expedition, Country, State_Province, Location, PI FROM boreholes"):
-        [e, c, s, f, p] = r
-        expeditions.add(e)
-
-        c = c.replace(',','') if c else None
-        s = s.replace(',','') if s else None
-        f = f.replace(',','') if f else None
-        p = p.split(', ') if p else None
-
-        if e in debug_projects:
-            print('expedition: {}\ncountry: {}\nstate: {}\nfeature name: {}\npis: {}\n'.format(e,c,s,f,p))
-        
-        c and countries[e].add(c)
-        s and states[e].add(s)
-        f and feature_names[e].add(f)
-        if p:
-            for pi in p:
-                if pi not in pis[e]:
-                    pis[e].append(pi)
-
-    with open(outfile, 'w', encoding='utf-8-sig') as f:
-        f.write('\"PROJECT ID\",\"LOCATION\",\"NAMED FEATURE\",\"INVESTIGATOR\"\n')
-
-        for e in sorted(expeditions-set(exclude_projects)):
-            l = ','.join(sorted(countries[e])+sorted(states[e]))
-            nf = ','.join(sorted(feature_names[e]))
-            i = ','.join(pis[e])
-
-            f.write('\"' + e + '\",\"' + l + '\",\"' + nf + '\",\"' + i + '\"\n')
-
-    print('{0} projects found.'.format(len(expeditions-set(exclude_projects))))
-    print('Aggregated data written to {0}.'.format(outfile))
+  print('{0} projects found.'.format(len(expeditions-set(exclude_projects))))
+  print('Aggregated data written to {0}.'.format(outfile))
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Aggregate fields from the CSDCO database by Expedition Drupal for publishing.')
-    parser.add_argument('db_file', type=str, help='Name of CSDCO database file.')
-    parser.add_argument('-f', type=str, help='Filename for export.')
-    args = parser.parse_args()
+  parser = argparse.ArgumentParser(description='Aggregate fields from the CSDCO database by Expedition Drupal for publishing.')
+  parser.add_argument('db_file', type=str, help='Name of CSDCO database file.')
+  parser.add_argument('-f', type=str, help='Filename for export.')
+  args = parser.parse_args()
 
-    if not os.path.isfile(args.db_file):
-        print('ERROR: database file \'{}\' does not exist.\n'.format(args.db_file))
-        exit(1)
+  if not os.path.isfile(args.db_file):
+    print('ERROR: database file \'{}\' does not exist.\n'.format(args.db_file))
+    exit(1)
 
-    # Use filename if provided, else create using datetimestamp
-    outfile = args.f if args.f else 'project_data_' + datetime.datetime.now().strftime('%Y%m%d_%H%M%S') + '.csv'
+  # Use filename if provided, else create using datetimestamp
+  outfile = args.f if args.f else 'project_data_' + datetime.datetime.now().strftime('%Y%m%d_%H%M%S') + '.csv'
 
-    # List of projects to exclude from export, e.g., ocean drilling projects
-    exclude_projects = ['AT15','ORCA','B0405','B0506','SBB']
-        
-    # List of projects to print info on for troubleshooting
-    debug_projects = []
+  # List of projects to exclude from export, e.g., ocean drilling projects
+  exclude_projects = ['AT15','ORCA','B0405','B0506','SBB']
+    
+  # List of projects to print info on for troubleshooting
+  debug_projects = []
 
-    start_time = timeit.default_timer()
-    aggregate_metadata(args.db_file, outfile, exclude_projects=exclude_projects, debug_projects=debug_projects)
-    end_time = timeit.default_timer()
-    print('Completed in {0} seconds.'.format(round(end_time-start_time,2)))
+  start_time = timeit.default_timer()
+  aggregate_metadata(args.db_file, outfile, exclude_projects=exclude_projects, debug_projects=debug_projects)
+  end_time = timeit.default_timer()
+  print('Completed in {0} seconds.'.format(round(end_time-start_time,2)))
